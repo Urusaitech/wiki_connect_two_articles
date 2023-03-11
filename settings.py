79772cdec5888e0e84d6c5ed22339b5a3fc14294
Urusaitech <urusai@engineer.com>
2023-03-11 19:53:53 +0300

    git init

--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
 start_url = 'https://ru.wikipedia.org/wiki/Xbox_360_S'
 end_url = 'https://ru.wikipedia.org/wiki/Nintendo_3DS'
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,3 +4,4 @@ from main import get_page_valid_links
 
 def test_get_valid_page_links():
     assert ...
+
